062cadbdc448844ff5de93a859698a5febf53607
xiaobu <xioobu@gmail.com>
2025-03-28 17:28:52 +0800

    feat:修改 web 端 host & port

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '127.0.0.1',
+    port: 8010,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
